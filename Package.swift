6fc0e4cc3484bb02c87151be141578ad34547cec
Niklas Sauer <nik.sauer@me.com>
2018-02-14 14:05:59 +0100

    updated false Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "RoadChatAPI",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-beta.3")),
-        .package(url: "https://github.com/vapor/fluent.git", .branch("3.0.0-beta.2")),
-        .package(url: "https://github.com/vapor/auth.git", .branch("2.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/fluent.git", .exact("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/auth.git", .exact("2.0.0-beta.2")),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentSQLite", "Authentication"]),
