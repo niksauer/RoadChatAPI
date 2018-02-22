558aa6a8d21a1e7cfd881970ce6b5e6e97277b0a
Nik Sauer <nik.sauer@me.com>
2018-02-22 18:53:39 +0100

    JSend specification (#103)
    * added message property to APIFail protocol, created JSend specification response wrapper, disabled array parameter presence check to avoid crashing
    
    * added JSend specification for errors

--- Sources/App/Middleware/APIFail.swift
+++ Sources/App/Middleware/APIFail.swift
@@ -7,20 +7,51 @@
 
 import Foundation
 
-protocol APIFail: Error { }
+protocol APIFail: Error {
+    var message: String { get }
+}
 
 enum RequestFail: APIFail {
     case missingParameters([String])
     case invalidTypeForParameters([String])
     case mismatchedContraints(Error)
+    
+    var message: String {
+        switch self {
+        case .missingParameters(let parameters):
+            return "Missing required parameters: \(parameters.description)"
+        case .invalidTypeForParameters(let parameters):
+            return "Invalid type for parameters: \(parameters)"
+        case .mismatchedContraints(let violations):
+            return "Mismatched constraints: \(violations)"
+        }
+    }
 }
 
 enum RegisterFail: APIFail {
     case emailTaken
     case usernameTaken
+    
+    var message: String {
+        switch self {
+        case .emailTaken:
+            return "This email address has already been registered."
+        case .usernameTaken:
+            return "This username has already been registered."
+        }
+    }
 }
 
 enum ConversationFail: APIFail {
     case invalidParticipants([Int])
     case minimumParticipants
+    
+    var message: String {
+        switch self {
+        case .invalidParticipants(let participants):
+            return "Invalid participants: \(participants)"
+        case .minimumParticipants:
+            return "A conversation must have at least one participant."
+        }
+    }
 }
