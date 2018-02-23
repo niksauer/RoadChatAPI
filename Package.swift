765854ff4bc51876371a6a6b312badde2d249391
Niklas Sauer <nik.sauer@me.com>
2018-02-23 14:19:21 +0100

    updated dependencies to use vapor repos

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", .exact("3.0.0-beta.3.1.3")),
         .package(url: "https://github.com/vapor/fluent", .exact("3.0.0-beta.3")),
-        .package(url: "https://github.com/niksauer/auth", .branch("beta")),
+        .package(url: "https://github.com/vapor/auth", .revision("3b2a5d8980e24089b250a46e1cca11c9951747b6")),
         .package(url: "https://github.com/niksauer/GeoSwift", .branch("master")),
     ],
     targets: [
