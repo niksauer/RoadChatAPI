44b148b0f5a770c00daabfb0f047feb5e52ccdfd
Niklas Sauer <nik.sauer@me.com>
2018-02-23 12:56:47 +0100

    updated packet to pinpoint fluent commit

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "RoadChatAPI",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", .exact("3.0.0-rc.1")),
-        .package(url: "https://github.com/vapor/fluent", .branch("master")),
+        .package(url: "https://github.com/vapor/fluent", .revision("7e5b7f69b88c018fe7b07293e3fc284cbb9060be")),
         .package(url: "https://github.com/niksauer/auth", .branch("beta")),
         .package(url: "https://github.com/niksauer/GeoSwift", .branch("master")),
     ],
