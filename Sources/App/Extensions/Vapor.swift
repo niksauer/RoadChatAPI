3f903b03fe2d247bbcbf2400fb89d86cd79a601a
Niklas Sauer <nik.sauer@me.com>
2018-04-15 00:28:25 +0200

    enable MySQLDataConvertible for URL and Data types

--- Sources/App/Extensions/Vapor.swift
+++ Sources/App/Extensions/Vapor.swift
@@ -6,51 +6,32 @@
 //
 
 import Foundation
-//import FluentSQLite
-//
-//enum SQLiteError: Error {
-//    case invalidBool
-//}
-//
-//extension Bool: SQLiteDataConvertible {
-//    public static func convertFromSQLiteData(_ data: SQLiteData) throws -> Bool {
-//        switch data {
-//        case .integer(let integer):
-//            let state = integer != 0 ? true : false
-//            return state
-//        default:
-//            throw SQLiteError.invalidBool
-//        }
-//    }
-//    
-//    public func convertToSQLiteData() throws -> SQLiteData {
-//        let state = self == false ? 0 : 1
-//        return SQLiteData.integer(state)
-//    }
-//}
-
 import FluentMySQL
 
 enum MySQLError: Error {
     case noDataAvailable
 }
 
-extension Data: MySQLDataConvertible {
+extension URL: MySQLColumnDefinitionStaticRepresentable {
+    public static var mySQLColumnDefinition: MySQLColumnDefinition {
+        return .text()
+    }
+}
+
+extension Data: MySQLColumnDefinitionStaticRepresentable, MySQLDataConvertible {
     public func convertToMySQLData() throws -> MySQLData {
         return MySQLData(data: self)
     }
-
+    
     public static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> Data {
         guard let data = mysqlData.data() else {
             throw MySQLError.noDataAvailable
         }
-
+        
         return data
     }
-}
 
-extension Data: MySQLColumnDefinitionStaticRepresentable {
     public static var mySQLColumnDefinition: MySQLColumnDefinition {
-        return .binary(length: 100)
+        return .blob(length: 1024)
     }
 }
